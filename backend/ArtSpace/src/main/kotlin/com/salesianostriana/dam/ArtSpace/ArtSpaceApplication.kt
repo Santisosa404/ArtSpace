b3dcf18b1ebe1b81c970da9b2ae9b51fb5af6790
santi sosa <santisosadiaz@gmail.com>
2021-03-15 15:57:58 +0100

    Generado proyeccto Spring Initializr

---
+++ backend/ArtSpace/src/main/kotlin/com/salesianostriana/dam/ArtSpace/ArtSpaceApplication.kt
@@ -0,0 +1,11 @@
+package com.salesianostriana.dam.ArtSpace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArtSpaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArtSpaceApplication>(*args)
+}
