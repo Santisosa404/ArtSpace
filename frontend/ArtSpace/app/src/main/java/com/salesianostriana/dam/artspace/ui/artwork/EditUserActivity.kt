7152c536fca95b81aea0b661835a84885fb6821e
santi sosa <santisosadiaz@gmail.com>
2021-03-24 21:12:09 +0100

    Editar perfil funciona

---
+++ frontend/ArtSpace/app/src/main/java/com/salesianostriana/dam/artspace/ui/artwork/EditUserActivity.kt
@@ -0,0 +1,11 @@
+package com.salesianostriana.dam.artspace.ui.artwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_user)
+    }
+}
